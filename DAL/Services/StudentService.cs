4f1fb3fffafcd8f6ee1699c6cf8f9be5f8ae6cff
Dj3ni <fernandezgarcia.jenny@gmail.com>
2025-03-05 16:20:59 +0100

    finished services Student DAL

--- DAL/Services/StudentService.cs
+++ DAL/Services/StudentService.cs
@@ -59,19 +59,74 @@ namespace DAL.Services
 			}
 		}
 
-		public int Insert(Student entity)
+		public int Insert(Student student)
 		{
-			throw new NotImplementedException();
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				using (SqlCommand command = connection.CreateCommand())
+				{
+					command.CommandText = "Insert INTO Student (first_name,last_name,birth_date,login,section_id,year_result,course_id) output Inserted.student_id Values (@first_name,@last_name,@birth_date,@login,@section_id,@year_result,@course_id)";
+					command.CommandType = System.Data.CommandType.Text;
+					//Parameters
+					command.Parameters.AddWithValue("first_name", (student.First_name is null)? DBNull.Value : student.First_name);
+					command.Parameters.AddWithValue("last_name", (student.Last_name is null)? DBNull.Value : student.Last_name);
+					command.Parameters.AddWithValue("birth_date", (student.Birth_date is null)? DBNull.Value : student.Birth_date);
+					command.Parameters.AddWithValue("login", (student.Login is null)? DBNull.Value : student.Login);
+					command.Parameters.AddWithValue("section_id", (student.Section_id is null)? DBNull.Value : student.Section_id);
+					command.Parameters.AddWithValue("year_result", (student.Year_result is null)? DBNull.Value : student.Year_result);
+					command.Parameters.AddWithValue("course_id", student.Course_id);
+
+					connection.Open();
+					return (int)command.ExecuteScalar();
+					
+
+				}
+			}
 		}
 
-		public void Update(int id, Student entity)
+		public void Update(int id, Student student)
 		{
-			throw new NotImplementedException();
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				using (SqlCommand command = connection.CreateCommand())
+				{
+					command.CommandText = 
+						"Update Table Student SET" +
+						"(first_name = @first_name,last_name = @last_name, birth_date = @birth_date, login = @login, section_id = @section_id, year_result = @year_result, course_id = @course_id) Where student_id = @student_id";
+					command.CommandType = System.Data.CommandType.Text;
+					//Parameters
+					command.Parameters.AddWithValue("first_name", (student.First_name is null) ? DBNull.Value : student.First_name);
+					command.Parameters.AddWithValue("last_name", (student.Last_name is null) ? DBNull.Value : student.Last_name);
+					command.Parameters.AddWithValue("birth_date", (student.Birth_date is null) ? DBNull.Value : student.Birth_date);
+					command.Parameters.AddWithValue("login", (student.Login is null) ? DBNull.Value : student.Login);
+					command.Parameters.AddWithValue("section_id", (student.Section_id is null) ? DBNull.Value : student.Section_id);
+					command.Parameters.AddWithValue("year_result", (student.Year_result is null) ? DBNull.Value : student.Year_result);
+					command.Parameters.AddWithValue("course_id", student.Course_id);
+
+					connection.Open();
+					command.ExecuteNonQuery();
+
+				}
+			}
 		}
 
 		public void Delete(int id)
 		{
-			throw new NotImplementedException();
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				using (SqlCommand command = connection.CreateCommand())
+				{
+					command.CommandText = "Delete From Student Where student_id = @student_id";
+					command.CommandType = System.Data.CommandType.Text;
+					//Parameters
+					command.Parameters.AddWithValue("student_id", id);
+
+					connection.Open();
+					command.ExecuteNonQuery();
+
+				}
+			}
 		}
 	}
+	
 }
